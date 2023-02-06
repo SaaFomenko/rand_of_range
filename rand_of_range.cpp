60da28c45e5129192bc395447e0bf9311177a45d
SaaFomenko <s.fomenko@list.ru>
2023-02-06 18:29:00 +0300

    First version save.

---
+++ rand_of_range.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h> 
+#include "rand_of_range.h"
+
+
+int rand_of_range(int min_num, int max_num)
+{
+	srand(time(NULL));
+
+  // Получить случайное число - формула
+  int num = min_num + rand() % (max_num - min_num + 1);
+
+  return num;
+}
---
+++ rand_of_range.h
@@ -0,0 +1,6 @@
+#ifndef RAND_OF_RANGE_H
+#define RAND_OF_RANGE_H
+
+int rand_of_range(int min_num, int max_num);
+
+#endif
